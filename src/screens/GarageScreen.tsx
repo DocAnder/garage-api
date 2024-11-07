95b785dc38b15bb4f580e28e8a8a3b07c7ff535c
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-11-07 11:03:08 -0300

    feat: adiciona componente de tela 'GarageScreen' para exibir os carros

---
+++ src/screens/GarageScreen.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+
+
+export default function GarageScreen(){
+    return (
+        <View>
+            <Text>
+                GarageScreen Works!
+            </Text>
+        </View>
+    )
+}
